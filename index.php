06a99754973b1909189cea555ef2a14f96a87bd3
Moobbot <ngotam24082001@gmail.com>
2021-09-15 21:47:40 +0700

    Khoi tao du an

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assests/css/style.css">
+    <link rel="stylesheet" href="assests/fonts/themify-icons.css">
+    <title>wowFood</title>
+</head>
+
+<body>
+    <div id="main">
+        <!-- Header Starts Here -->
+        <div id="header">
+            <div class="nav container">
+                <div class="logo">
+                    <a href="#" title="Logo">
+                        <img src="assests/img/Logo.PNG" alt="Restaurant Logo">
+                    </a>
+                </div>
+                <div class="nav-item flex-center">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+
+                        <li><a href="#">Categories</a></li>
+
+                        <li><a href="#">Foods</a></li>
+
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Header Ends Here -->
+        <div class="body">
+            <div class="content-header flex-center">
+                <input type="text" name="search" placeholder="Search for Food..." requireds>
+                <input class="btn btn-primary" type="submit" name="submit" value="Search">
+            </div>
+            <div class="content-foods">
+                <h1 class="text-center">Explore Foods</h1>
+            </div>
+            <div class="content-menu">
+                <h1 class="text-center">Food menu</h1>
+            </div>
+        </div>
+        <div class="footer"></div>
+
+    </div>
+
+</body>
+
+</html>
